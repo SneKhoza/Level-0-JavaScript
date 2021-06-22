5b71f775749e77afd79bbdb8bba24ca7c69294c7
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-06-22 03:25:59 +0200

    Task 6 commited

---
+++ Task_0-6.js
@@ -0,0 +1,10 @@
+function findMax() {
+    var maxNum = arguments[0];
+    for (var i = 0; i < arguments.length; i++) {
+        if (arguments[i] > maxNum) {
+            maxNum = arguments[i];
+        }
+    }
+    return console.log(maxNum);
+}
+findMax(1, 7, 3, 2, 6);
