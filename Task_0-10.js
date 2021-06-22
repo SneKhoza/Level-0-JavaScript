44eaa5e242dba5b7038479d83f56edc0913f9e93
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-06-22 02:26:39 +0200

    Task 10 commited

---
+++ Task_0-10.js
@@ -0,0 +1,26 @@
+function commonCharacter(string1, string2) {
+    var str1 = string1.split('');
+    var str2 = string2.split('');
+    var sorted1 = str1.concat().sort();
+    var sorted2 = str2.concat().sort();
+    var common = [];
+    var commonString;
+    var i = 0;
+    var j = 0;
+
+    while (i < str1.length && j < str2.length) {
+        if (sorted1[i] === sorted2[j]) {
+            common.push(sorted1[i])
+            commonString = common.toString();
+            i++;
+            j++;
+        } else if (sorted1[i] < sorted2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    return console.log("Common letters: " + commonString);
+}
+
+commonCharacter("house", "computers");
