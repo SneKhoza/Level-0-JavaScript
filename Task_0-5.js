0ae04793d66f7ec317a4f1e19713dc0194f9b007
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-06-24 18:06:04 +0200

    modified task 5

--- Task_0-5.js
+++ Task_0-5.js
@@ -1,6 +1,6 @@
 function triangle(num1, num2, num3) {
     var semiP = (1 / 2) * (num1 + num2 + num3); //Calculating semiperimeter
     var area = Math.round(Math.sqrt(semiP * (semiP - num1) * (semiP - num2) * (semiP - num3))); //Heron's formula
-    return console.log(area);
+    return area;
 }
 triangle(5, 7, 4)
