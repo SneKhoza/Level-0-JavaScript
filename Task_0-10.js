11f2b0781be6ce6f6811407147c0092f4d42d990
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-08-03 02:33:55 +0200

    Task 10 further modified

--- Task_0-10.js
+++ Task_0-10.js
@@ -1,16 +1,15 @@
 function commonCharacter(string1, string2) {
-    var str1 = string1.split('');
-    var str2 = string2.split('');
-    var sorted1 = str1.concat().sort();
-    var sorted2 = str2.concat().sort();
-    var common = [];
-    var commonString;
-    var i = 0;
-    var j = 0;
+    const str1 = string1.split("");
+    const str2 = string2.split("");
+    const sorted1 = str1.concat().sort();
+    const sorted2 = str2.concat().sort();
+    const common = [];
+    let i = 0;
+    let j = 0;
 
     while (i < str1.length && j < str2.length) {
         if (sorted1[i] === sorted2[j]) {
-            common.push(sorted1[i])
+            common.push(sorted1[i]);
             i++;
             j++;
         } else if (sorted1[i] < sorted2[j]) {
@@ -19,8 +18,8 @@ function commonCharacter(string1, string2) {
             j++;
         }
     }
-    commonString = common.toString();
-    console.log("Common letters: " + commonString);
+    const commonString = common.join(", ");
+    console.log(`Common letters: ${commonString}`);
 }
 
 commonCharacter("computers", "house");
