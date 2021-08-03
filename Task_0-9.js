612ef39490309356855d340df266a7b51ff557a3
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-08-03 02:33:26 +0200

    Task 9 further modified

--- Task_0-9.js
+++ Task_0-9.js
@@ -1,18 +1,16 @@
 function findVowels(vowelString) {
-    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    var LowercaseString = vowelString.toLowerCase();
-    var stringArray = LowercaseString.split('');
-    var vowelSorted = vowels.concat().sort();
-    var stringArraySorted = stringArray.concat().sort();
-    var vowelsFound = [];
-    var CompleteString;
-    var i = 0;
-    var j = 0;
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    const lowercaseString = vowelString.toLowerCase();
+    const stringArray = lowercaseString.split("");
+    const vowelSorted = vowels.concat().sort();
+    const stringArraySorted = stringArray.concat().sort();
+    const vowelsFound = [];
+    let i = 0;
+    let j = 0;
 
     while (i < stringArray.length && j < vowels.length) {
         if (stringArraySorted[i] === vowelSorted[j]) {
             vowelsFound.push(stringArraySorted[i]);
-            CompleteString = vowelsFound.toString();
             i++;
             j++;
         } else if (stringArraySorted[i] < vowelSorted[j]) {
@@ -22,14 +20,14 @@ function findVowels(vowelString) {
         }
     }
 
-    var newArray = [vowelsFound[0]];
-    for (var k = 1; k < vowelsFound.length; k++) {
-        if (vowelsFound[k] != vowelsFound[k - 1]) {
+    const newArray = [vowelsFound[0]];
+    for (let k = 1; k < vowelsFound.length; k++) {
+        if (vowelsFound[k] !== vowelsFound[k - 1]) {
             newArray.push(vowelsFound[k]);
         }
     }
-    var newString = newArray.toString();
-    console.log("Vowels: " + newString);
+    const newString = newArray.join(", ");
+    console.log(`Vowels: ${newString}`);
 }
 
 findVowels("Umuzi");
