d7c9c2f2f3b511f5d4e133b7cdd40c65f979e0ba
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-08-03 02:32:44 +0200

    Task 8 further modified

--- Task_0-8.js
+++ Task_0-8.js
@@ -1,16 +1,16 @@
 function convertNumToTime(num) {
-    var timeString
-    var hour = Math.floor(num / 60);
-    var minute = num % 60;
+    let timeString;
+    const hour = Math.floor(num / 60);
+    const minute = num % 60;
 
-    if ((hour > 1 || hour === 0) && (minute > 1 || minute === 0)) { //0 hours and 0 minutes are plural
-        timeString = hour + " hours, " + minute + " minutes.";
+    if ((hour > 1 || hour === 0) && (minute > 1 || minute === 0)) {
+        timeString = `${hour} hours, ${minute} minutes.`;
     } else if ((hour > 1 || hour === 0) && minute === 1) {
-        timeString = hour + " hours, " + minute + " minute.";
+        timeString = `${hour} hours, ${minute} minute.`;
     } else if (hour === 1 && (minute > 1 || minute === 0)) {
-        timeString = hour + " hour, " + minute + " minutes.";
+        timeString = `${hour} hour, ${minute} minutes.`;
     } else {
-        timeString = hour + " hour, " + minute + " minute.";
+        timeString = `${hour} hour, ${minute} minute.`;
     }
     console.log(timeString);
 }
