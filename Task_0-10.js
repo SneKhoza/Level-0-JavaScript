7065ef720892b1cc0dbb721753059dd36db4896f
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-08-15 23:53:45 +0200

    Correct output order

--- Task_0-10.js
+++ Task_0-10.js
@@ -1,25 +1,13 @@
 function commonCharacter(string1, string2) {
-    const str1 = string1.split("");
-    const str2 = string2.split("");
-    const sorted1 = str1.concat().sort();
-    const sorted2 = str2.concat().sort();
     const common = [];
-    let i = 0;
-    let j = 0;
-
-    while (i < str1.length && j < str2.length) {
-        if (sorted1[i] === sorted2[j]) {
-            common.push(sorted1[i]);
-            i++;
-            j++;
-        } else if (sorted1[i] < sorted2[j]) {
-            i++;
-        } else {
-            j++;
+    const length = Math.max(string1.length, string2.length);
+    for (let i = 0; i < length; i++) {
+        if (string1.includes(string2[i])) {
+            common.push(string2[i]);
         }
     }
     const commonString = common.join(", ");
     console.log(`Common letters: ${commonString}`);
 }
 
-commonCharacter("computers", "house");
+commonCharacter("house", "computers");
