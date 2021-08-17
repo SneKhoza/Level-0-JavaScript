3734bdb27cf40bc997ec8e0818585c21efd04670
Sinethemba Khoza <sinethembatnkhoza@gmail.com>
2021-08-17 21:42:17 +0200

    Added symbols

--- Task_0-7.js
+++ Task_0-7.js
@@ -1,11 +1,11 @@
 function celsiusToFahrenheit(numCelsius) {
     const numFahrenheit = numCelsius * (9 / 5) + 32;
-    return numFahrenheit;
+    return `${numFahrenheit} \u2109`;
 }
 console.log(celsiusToFahrenheit(2));
 
 function fahrenheitToCelsius(numFahrenheit) {
     const numCelsius = ((numFahrenheit - 32) * 5) / 9;
-    return numCelsius;
+    return `${numCelsius} \u2103`;
 }
 console.log(fahrenheitToCelsius(35));
